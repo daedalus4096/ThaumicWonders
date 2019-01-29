fcde27630e80e6dbf1080c193c24234ea94d33c3
Michael Bunting <daedalus4096@gmail.com>
2019-01-28 19:51:37 -0800

    Added end hedge alchemy

--- src/main/java/com/verdantartifice/thaumicwonders/common/recipes/ModRecipes.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/recipes/ModRecipes.java
@@ -33,5 +33,23 @@ public class ModRecipes {
                 new ItemStack(Items.SKULL, 1, 0),
                 new AspectList().add(Aspect.UNDEAD, 10).add(Aspect.DEATH, 10).add(Aspect.SOUL, 10).add(Aspect.DARKNESS, 5)
         ));
+        ThaumcraftApi.addCrucibleRecipe(new ResourceLocation("thaumicwonders", "hedge_end_stone"), new CrucibleRecipe(
+                "TWOND_END_HEDGE",
+                new ItemStack(Blocks.END_STONE),
+                new ItemStack(Blocks.STONE),
+                new AspectList().add(Aspect.EARTH, 5).add(Aspect.DARKNESS, 5)
+        ));
+        ThaumcraftApi.addCrucibleRecipe(new ResourceLocation("thaumicwonders", "hedge_chorus_fruit"), new CrucibleRecipe(
+                "TWOND_END_HEDGE",
+                new ItemStack(Items.CHORUS_FRUIT),
+                new ItemStack(Items.APPLE),
+                new AspectList().add(Aspect.ELDRITCH, 5).add(Aspect.SENSES, 5).add(Aspect.PLANT, 5)
+        ));
+        ThaumcraftApi.addCrucibleRecipe(new ResourceLocation("thaumicwonders", "hedge_ender_pearl"), new CrucibleRecipe(
+                "TWOND_END_HEDGE",
+                new ItemStack(Items.ENDER_PEARL, 2),
+                new ItemStack(Items.ENDER_PEARL),
+                new AspectList().add(Aspect.MOTION, 15).add(Aspect.ELDRITCH, 10)
+        ));
     }
 }
