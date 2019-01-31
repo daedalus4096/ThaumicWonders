5c8f8c3b26207c78f7f1913fa858b6a4f59edacd
Michael Bunting <daedalus4096@gmail.com>
2019-01-30 22:12:58 -0800

    Stub functionality for Everburning Urn.

--- src/main/java/com/verdantartifice/thaumicwonders/common/blocks/devices/BlockLavaJug.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/blocks/devices/BlockLavaJug.java
@@ -5,10 +5,53 @@ import com.verdantartifice.thaumicwonders.common.tiles.devices.TileLavaJug;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockLavaJug extends BlockDeviceTW<TileLavaJug> {
     public BlockLavaJug() {
         super(Material.ROCK, TileLavaJug.class, "everburning_urn");
         setSoundType(SoundType.STONE);
     }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+    
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return new AxisAlignedBB(0.1875D, 0.0D, 0.1875D, 0.8125D, 1.0D, 0.8125D);
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (!worldIn.isRemote) {
+            worldIn.playSound(null, pos, SoundEvents.ITEM_BUCKET_FILL_LAVA, SoundCategory.BLOCKS, 0.33F, 
+                    1.0F + (2F * worldIn.rand.nextFloat() - 1.0F) * 0.3F);
+        }
+        return true;
+    }
+
+    
 }
