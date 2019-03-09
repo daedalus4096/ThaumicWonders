2a547b3a6d9775c4354e77cab7b3c6006e038187
Michael Bunting <michael.a.bunting@gmail.com>
2019-03-08 22:54:02 -0800

    Do time advance, center buttons

--- src/main/java/com/verdantartifice/thaumicwonders/client/gui/GuiTimewinder.java
+++ src/main/java/com/verdantartifice/thaumicwonders/client/gui/GuiTimewinder.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.lwjgl.opengl.GL11;
 
 import com.verdantartifice.thaumicwonders.ThaumicWonders;
+import com.verdantartifice.thaumicwonders.common.network.PacketHandler;
+import com.verdantartifice.thaumicwonders.common.network.packets.PacketTimewinderAction;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
@@ -28,28 +30,35 @@ public class GuiTimewinder extends GuiScreen {
             this.mc = Minecraft.getMinecraft();
         }
         this.buttonList.clear();
-        // FIXME fix x and y positions to center GUI
-        this.buttonList.add(new GuiSelectorButton(0, 120, 88, 16, 16, I18n.format("thaumicwonders.gui.timewinder.0")));
-        this.buttonList.add(new GuiSelectorButton(1, 143, 97, 16, 16, I18n.format("thaumicwonders.gui.timewinder.1")));
-        this.buttonList.add(new GuiSelectorButton(2, 152, 120, 16, 16, I18n.format("thaumicwonders.gui.timewinder.2")));
-        this.buttonList.add(new GuiSelectorButton(3, 143, 143, 16, 16, I18n.format("thaumicwonders.gui.timewinder.3")));
-        this.buttonList.add(new GuiSelectorButton(4, 120, 152, 16, 16, I18n.format("thaumicwonders.gui.timewinder.4")));
-        this.buttonList.add(new GuiSelectorButton(8, 120, 120, 16, 16, I18n.format("thaumicwonders.gui.timewinder.8")));    // out of order for z-ordering
-        this.buttonList.add(new GuiSelectorButton(5, 97, 143, 16, 16, I18n.format("thaumicwonders.gui.timewinder.5")));
-        this.buttonList.add(new GuiSelectorButton(6, 88, 120, 16, 16, I18n.format("thaumicwonders.gui.timewinder.6")));
-        this.buttonList.add(new GuiSelectorButton(7, 97, 97, 16, 16, I18n.format("thaumicwonders.gui.timewinder.7")));
+
+        int baseX = (this.width - 16) / 2;
+        int baseY = (this.height - 16) / 2;
+        this.buttonList.add(new GuiSelectorButton(0, baseX, baseY - 32, 120, 88, 16, 16, I18n.format("thaumicwonders.gui.timewinder.0")));
+        this.buttonList.add(new GuiSelectorButton(1, baseX + 23, baseY - 23, 143, 97, 16, 16, I18n.format("thaumicwonders.gui.timewinder.1")));
+        this.buttonList.add(new GuiSelectorButton(2, baseX + 32, baseY, 152, 120, 16, 16, I18n.format("thaumicwonders.gui.timewinder.2")));
+        this.buttonList.add(new GuiSelectorButton(3, baseX + 23, baseY + 23, 143, 143, 16, 16, I18n.format("thaumicwonders.gui.timewinder.3")));
+        this.buttonList.add(new GuiSelectorButton(4, baseX, baseY + 32, 120, 152, 16, 16, I18n.format("thaumicwonders.gui.timewinder.4")));
+        this.buttonList.add(new GuiSelectorButton(8, baseX, baseY, 120, 120, 16, 16, I18n.format("thaumicwonders.gui.timewinder.8")));    // out of order for z-ordering
+        this.buttonList.add(new GuiSelectorButton(5, baseX - 23, baseY + 23, 97, 143, 16, 16, I18n.format("thaumicwonders.gui.timewinder.5")));
+        this.buttonList.add(new GuiSelectorButton(6, baseX - 32, baseY, 88, 120, 16, 16, I18n.format("thaumicwonders.gui.timewinder.6")));
+        this.buttonList.add(new GuiSelectorButton(7, baseX - 23, baseY - 23, 97, 97, 16, 16, I18n.format("thaumicwonders.gui.timewinder.7")));
     }
     
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
-        ThaumicWonders.LOGGER.info("Clicked button {}!", button.id);
+        PacketHandler.INSTANCE.sendToServer(new PacketTimewinderAction(button.id));
+        this.mc.player.closeScreen();
     }
     
     private class GuiSelectorButton extends GuiButton {
         private final ResourceLocation TEXTURE = new ResourceLocation(ThaumicWonders.MODID, "textures/gui/gui_timewinder.png");
+        private int texX;
+        private int texY;
         
-        public GuiSelectorButton(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText) {
+        public GuiSelectorButton(int buttonId, int x, int y, int texX, int texY, int widthIn, int heightIn, String buttonText) {
             super(buttonId, x, y, widthIn, heightIn, buttonText);
+            this.texX = texX;
+            this.texY = texY;
         }
         
         @Override
@@ -66,7 +75,7 @@ public class GuiTimewinder extends GuiScreen {
                 } else {
                     GL11.glColor4f(0.8F, 0.8F, 0.8F, 1.0F);
                 }
-                this.drawTexturedModalRect(this.x, this.y, this.x, this.y, 16, 16);
+                this.drawTexturedModalRect(this.x, this.y, this.texX, this.texY, 16, 16);
                 GL11.glPopMatrix();
                 if (this.hovered) {
                     this.drawString(mc.fontRenderer, this.displayString, this.x + 19, this.y + 4, 16777215);
--- src/main/java/com/verdantartifice/thaumicwonders/common/network/PacketHandler.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/network/PacketHandler.java
@@ -1,13 +1,22 @@
 package com.verdantartifice.thaumicwonders.common.network;
 
 import com.verdantartifice.thaumicwonders.ThaumicWonders;
+import com.verdantartifice.thaumicwonders.common.network.packets.PacketTimewinderAction;
 
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class PacketHandler {
     public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(ThaumicWonders.MODID);
     
+    private static int packetId = 0;
+    
+    private static int nextId() {
+        return packetId++;
+    }
+    
     public static void registerMessages() {
+        INSTANCE.registerMessage(PacketTimewinderAction.Handler.class, PacketTimewinderAction.class, nextId(), Side.SERVER);
     }
 }
