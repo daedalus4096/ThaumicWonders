7658de24697a32ccfc8512311860ff6bf8b2bb28
Michael Bunting <michael.a.bunting@gmail.com>
2019-03-12 22:21:20 -0700

    Derive from ItemClock instead of reimplementing time property

--- src/main/java/com/verdantartifice/thaumicwonders/common/items/misc/ItemTimewinder.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/items/misc/ItemTimewinder.java
@@ -1,75 +1,27 @@
 package com.verdantartifice.thaumicwonders.common.items.misc;
 
-import javax.annotation.Nullable;
-
 import com.verdantartifice.thaumicwonders.ThaumicWonders;
-import com.verdantartifice.thaumicwonders.common.items.base.ItemTW;
 import com.verdantartifice.thaumicwonders.common.misc.GuiIds;
 
 import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.IItemPropertyGetter;
+import net.minecraft.item.ItemClock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemTimewinder extends ItemTW {
+public class ItemTimewinder extends ItemClock {
     public ItemTimewinder() {
-        super("timewinder");
+        super();
+        setRegistryName(ThaumicWonders.MODID, "timewinder");
+        setUnlocalizedName(ThaumicWonders.MODID + "." + this.getRegistryName().getResourcePath());
+        setCreativeTab(ThaumicWonders.CREATIVE_TAB);
         setMaxStackSize(1);
         setNoRepair();
-        
-        this.addPropertyOverride(new ResourceLocation("time"), new IItemPropertyGetter()
-        {
-            @SideOnly(Side.CLIENT)
-            private double rotation;
-            @SideOnly(Side.CLIENT)
-            private double rota;
-            @SideOnly(Side.CLIENT)
-            private long lastUpdateTick;
-            @SideOnly(Side.CLIENT)
-            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
-                Entity entity = (Entity)(entityIn != null ? entityIn : stack.getItemFrame());
-                if (worldIn == null && entity != null) {
-                    worldIn = entity.world;
-                }
-
-                if (worldIn == null) {
-                    return 0.0F;
-                } else {
-                    double d0;
-                    if (worldIn.provider.isSurfaceWorld()) {
-                        d0 = (double)worldIn.getCelestialAngle(1.0F);
-                    } else {
-                        d0 = Math.random();
-                    }
-                    d0 = this.wobble(worldIn, d0);
-                    return (float)d0;
-                }
-            }
-            @SideOnly(Side.CLIENT)
-            private double wobble(World worldIn, double angle) {
-                if (worldIn.getTotalWorldTime() != this.lastUpdateTick) {
-                    this.lastUpdateTick = worldIn.getTotalWorldTime();
-                    double d0 = angle - this.rotation;
-                    d0 = MathHelper.positiveModulo(d0 + 0.5D, 1.0D) - 0.5D;
-                    this.rota += d0 * 0.1D;
-                    this.rota *= 0.9D;
-                    this.rotation = MathHelper.positiveModulo(this.rotation + this.rota, 1.0D);
-                }
-                return this.rotation;
-            }
-        });
     }
     
     @Override
