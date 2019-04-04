c0a1a82d69bcca97a49c4caa13858fb83053c19c
Michael Bunting <michael.a.bunting@gmail.com>
2019-04-03 20:45:17 -0700

    Fixed other issues of I18N on server

--- src/main/java/com/verdantartifice/thaumicwonders/common/network/PacketHandler.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/network/PacketHandler.java
@@ -4,6 +4,8 @@ import com.verdantartifice.thaumicwonders.ThaumicWonders;
 import com.verdantartifice.thaumicwonders.common.network.packets.PacketPrimalDestroyerHungerFull;
 import com.verdantartifice.thaumicwonders.common.network.packets.PacketTimewinderAction;
 import com.verdantartifice.thaumicwonders.common.network.packets.PacketTimewinderUsed;
+import com.verdantartifice.thaumicwonders.common.network.packets.PacketVoidPortalNoAnchor;
+import com.verdantartifice.thaumicwonders.common.network.packets.PacketVoidPortalNoWorld;
 
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
@@ -22,5 +24,7 @@ public class PacketHandler {
         INSTANCE.registerMessage(PacketTimewinderAction.Handler.class, PacketTimewinderAction.class, nextId(), Side.SERVER);
         INSTANCE.registerMessage(PacketTimewinderUsed.Handler.class, PacketTimewinderUsed.class, nextId(), Side.CLIENT);
         INSTANCE.registerMessage(PacketPrimalDestroyerHungerFull.Handler.class, PacketPrimalDestroyerHungerFull.class, nextId(), Side.CLIENT);
+        INSTANCE.registerMessage(PacketVoidPortalNoAnchor.Handler.class, PacketVoidPortalNoAnchor.class, nextId(), Side.CLIENT);
+        INSTANCE.registerMessage(PacketVoidPortalNoWorld.Handler.class, PacketVoidPortalNoWorld.class, nextId(), Side.CLIENT);
     }
 }
