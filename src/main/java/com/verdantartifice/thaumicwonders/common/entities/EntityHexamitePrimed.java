9d7866f2387eb4fe24e0738846aa92bb2e8ff60e
Michael Bunting <michael.a.bunting@gmail.com>
2019-04-06 23:59:28 -0700

    Linked block to entity

--- src/main/java/com/verdantartifice/thaumicwonders/common/entities/EntityHexamitePrimed.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/entities/EntityHexamitePrimed.java
@@ -1,7 +1,11 @@
 package com.verdantartifice.thaumicwonders.common.entities;
 
+import com.verdantartifice.thaumicwonders.ThaumicWonders;
+
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.MoverType;
 import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 
 public class EntityHexamitePrimed extends EntityTNTPrimed {
@@ -15,11 +19,40 @@ public class EntityHexamitePrimed extends EntityTNTPrimed {
 
     @Override
     public void onUpdate() {
-        // TODO Auto-generated method stub
-        super.onUpdate();
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
+
+        if (!this.hasNoGravity()) {
+            this.motionY -= 0.04D;
+        }
+
+        this.move(MoverType.SELF, this.motionX, this.motionY, this.motionZ);
+        this.motionX *= 0.98D;
+        this.motionY *= 0.98D;
+        this.motionZ *= 0.98D;
+
+        if (this.onGround) {
+            this.motionX *= 0.7D;
+            this.motionZ *= 0.7D;
+            this.motionY *= -0.5D;
+        }
+
+        this.setFuse(this.getFuse() - 1);
+
+        if (this.getFuse() <= 0) {
+            this.setDead();
+            if (!this.world.isRemote) {
+                this.explode();
+            }
+        } else {
+            this.handleWaterMovement();
+            this.world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, this.posX, this.posY + 0.5D, this.posZ, 0.0D, 0.0D, 0.0D);
+        }
     }
     
     protected void explode() {
-        // TODO create explosion object and process it
+        ThaumicWonders.LOGGER.info("BOOM");
+        this.setDead();
     }
 }
