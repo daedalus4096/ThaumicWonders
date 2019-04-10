63b527dd9ad0abb7ba263cd9253fa33cf55e4142
Michael Bunting <michael.a.bunting@gmail.com>
2019-04-09 18:19:05 -0700

    Update version to 1.3.0

--- src/main/java/com/verdantartifice/thaumicwonders/ThaumicWonders.java
+++ src/main/java/com/verdantartifice/thaumicwonders/ThaumicWonders.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ThaumicWonders {
     public static final String MODID = "thaumicwonders";
     public static final String NAME = "Thaumic Wonders";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
     public static final String DEPENDENCIES = "required-after:thaumcraft";
 
     public static final CreativeTabs CREATIVE_TAB = new CreativeTabTW(CreativeTabs.getNextID(), ThaumicWonders.MODID);
