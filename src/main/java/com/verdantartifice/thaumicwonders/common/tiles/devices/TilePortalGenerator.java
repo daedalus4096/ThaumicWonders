b6d9a111c30384f560b281f017cd4d071cf3c194
Michael Bunting <michael.a.bunting@gmail.com>
2019-04-19 22:05:43 -0700

    Added spark effects to portal generator

--- src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TilePortalGenerator.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TilePortalGenerator.java
@@ -1,5 +1,6 @@
 package com.verdantartifice.thaumicwonders.common.tiles.devices;
 
+import java.awt.Color;
 import java.util.List;
 
 import com.verdantartifice.thaumicwonders.ThaumicWonders;
@@ -7,15 +8,20 @@ import com.verdantartifice.thaumicwonders.common.entities.EntityVoidPortal;
 import com.verdantartifice.thaumicwonders.common.tiles.base.TileTW;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.client.fx.FXDispatcher;
 import thaumcraft.common.lib.SoundsTC;
 
-public class TilePortalGenerator extends TileTW {
+public class TilePortalGenerator extends TileTW implements ITickable {
     protected int linkX = 0;
     protected int linkY = 0;
     protected int linkZ = 0;
     protected int linkDim = 0;
+    protected int counter = 0;
     
     @Override
     protected void readFromTileNBT(NBTTagCompound compound) {
@@ -66,6 +72,26 @@ public class TilePortalGenerator extends TileTW {
             } else {
                 ThaumicWonders.LOGGER.warn("Couldn't find void portal entity to kill!");
             }
+        }
+    }
+
+    @Override
+    public void update() {
+        this.counter++;
+        if (this.world.isRemote && this.counter == 20) {
+            BlockPos sourcePos = this.pos.up();
+            if (this.world.rand.nextInt() % 2 == 0) {
+                sourcePos = sourcePos.south();
+            }
+            if (this.world.rand.nextInt() % 2 == 0) {
+                sourcePos = sourcePos.east();
+            }
+            Color color = new Color(Aspect.FLUX.getColor());
+            float r = color.getRed() / 255.0F;
+            float g = color.getGreen() / 255.0F;
+            float b = color.getBlue() / 255.0F;
+            FXDispatcher.INSTANCE.drawLightningFlash(sourcePos.getX(), sourcePos.getY(), sourcePos.getZ(), r, g, b, 1.0F, 2.5F);
+            this.counter -= (20 + this.world.rand.nextInt(80));
         }
     }
 }
