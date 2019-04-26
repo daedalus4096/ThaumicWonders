cf9c10c3c69dfcd77e77c866ae43cda266467ac9
Michael Bunting <michael.a.bunting@gmail.com>
2019-04-25 19:19:51 -0700

    Update version to 1.4.0

--- src/main/java/com/verdantartifice/thaumicwonders/ThaumicWonders.java
+++ src/main/java/com/verdantartifice/thaumicwonders/ThaumicWonders.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ThaumicWonders {
     public static final String MODID = "thaumicwonders";
     public static final String NAME = "Thaumic Wonders";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
     public static final String DEPENDENCIES = "required-after:thaumcraft";
     public static final String UPDATE_URL = "https://pastebin.com/raw/7QELk9HJ";
 
