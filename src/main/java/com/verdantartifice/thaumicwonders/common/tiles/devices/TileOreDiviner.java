13cc87c22caa344cb3675ee25b7c19d77bb7c0c7
Michael Bunting <michael.a.bunting@gmail.com>
2019-05-13 23:04:50 -0700

    Added color to ore ping

--- src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TileOreDiviner.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TileOreDiviner.java
@@ -4,8 +4,14 @@ import java.awt.Color;
 
 import javax.annotation.Nullable;
 
+import com.verdantartifice.thaumicwonders.ThaumicWonders;
+import com.verdantartifice.thaumicwonders.common.misc.OreHelper;
 import com.verdantartifice.thaumicwonders.common.tiles.base.TileTW;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -49,6 +55,50 @@ public class TileOreDiviner extends TileTW implements ITickable {
     }
     
     protected Color getOreColor(BlockPos targetPos) {
-        return new Color(0xC0C0C0);
+        ItemStack stack = ItemStack.EMPTY;
+        IBlockState state = world.getBlockState(targetPos);
+        if (state.getBlock() != Blocks.AIR && state.getBlock() != Blocks.BEDROCK) {
+            Item item = Item.getItemFromBlock(state.getBlock());
+            stack = new ItemStack(item, 1, item.getHasSubtypes() ? state.getBlock().getMetaFromState(state) : 0);
+        }
+        if (stack.isEmpty() || stack.getItem() == null) {
+            return new Color(0xC0C0C0);
+        } else {
+            for (String name : OreHelper.getOreNames(stack)) {
+                if (name != null) {
+                    name = name.toUpperCase();
+                    if (name.contains("IRON")) {
+                        return new Color(0xD8AF93);
+                    } else if (name.contains("COAL")) {
+                        return new Color(0x101010);
+                    } else if (name.contains("REDSTONE")) {
+                        return new Color(0xFF0000);
+                    } else if (name.contains("GOLD")) {
+                        return new Color(0xFCEE4B);
+                    } else if (name.contains("LAPIS")) {
+                        return new Color(0x1445BC);
+                    } else if (name.contains("DIAMOND")) {
+                        return new Color(0x5DECF5);
+                    } else if (name.contains("EMERALD")) {
+                        return new Color(0x17DD62);
+                    } else if (name.contains("QUARTZ")) {
+                        return new Color(0xE5DED5);
+                    } else if (name.contains("SILVER")) {
+                        return new Color(0xDAD9FD);
+                    } else if (name.contains("LEAD")) {
+                        return new Color(0x6B6B6B);
+                    } else if (name.contains("TIN")) {
+                        return new Color(0xEFEFFB);
+                    } else if (name.contains("COPPER")) {
+                        return new Color(0xFD9C55);
+                    } else if (name.contains("AMBER")) {
+                        return new Color(0xFDB325);
+                    } else if (name.contains("CINNABAR")) {
+                        return new Color(0x9B0508);
+                    }
+                }
+            }
+            return new Color(0xC0C0C0);
+        }
     }
 }
