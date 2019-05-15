bf9cda12a0c6fa14f77af54e085bd90c4d77762e
Michael Bunting <michael.a.bunting@gmail.com>
2019-05-14 22:38:08 -0700

    Rotate entity shown within diviner

--- src/main/java/com/verdantartifice/thaumicwonders/client/renderers/tile/TesrOreDiviner.java
+++ src/main/java/com/verdantartifice/thaumicwonders/client/renderers/tile/TesrOreDiviner.java
@@ -18,9 +18,11 @@ public class TesrOreDiviner extends TileEntitySpecialRenderer<TileOreDiviner> {
     public void render(TileOreDiviner te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
         super.render(te, x, y, z, partialTicks, destroyStage, alpha);
         if (te != null && te.getWorld() != null && te.getSearchStack() != null && !te.getSearchStack().isEmpty()) {
+            int rem = (int)(te.getWorld().getWorldTime() % 360);
             GL11.glPushMatrix();
             GL11.glTranslated(x + 0.5D, y + 0.0625D, z + 0.5D);
             GL11.glScaled(0.75D, 0.75D, 0.75D);
+            GL11.glRotated(rem, 0.0D, 1.0D, 0.0D);
             
             ItemStack stack = te.getSearchStack().copy();
             stack.setCount(1);
