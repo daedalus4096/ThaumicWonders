ce0e853ecb6c0a02d18c2d73fcf38b077ddb2132
Michael Bunting <michael.a.bunting@gmail.com>
2019-05-14 18:24:46 -0700

    Provide direction hint in diviner message

--- src/main/java/com/verdantartifice/thaumicwonders/common/network/packets/PacketOreDivinerSearch.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/network/packets/PacketOreDivinerSearch.java
@@ -19,6 +19,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
@@ -77,7 +78,9 @@ public class PacketOreDivinerSearch implements IMessage {
                     if (target == null) {
                         entityPlayer.sendStatusMessage(new TextComponentString(TextFormatting.DARK_PURPLE + I18n.format("event.ore_diviner.not_found")), true);
                     } else {
-                        entityPlayer.sendStatusMessage(new TextComponentString(TextFormatting.DARK_PURPLE + I18n.format("event.ore_diviner.found")), true);
+                        BlockPos delta = target.subtract(message.origin);
+                        EnumFacing facing = EnumFacing.getFacingFromVector(delta.getX(), delta.getY(), delta.getZ());
+                        entityPlayer.sendStatusMessage(new TextComponentString(TextFormatting.DARK_PURPLE + I18n.format("event.ore_diviner.found." + facing.getName())), true);
                     }
                     ((TileOreDiviner)tile).setTarget(target);
                 }
