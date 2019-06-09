28f5db6483e2257f0b4968a10d485dd852dd4e97
Michael Bunting <michael.a.bunting@gmail.com>
2019-06-08 23:30:07 -0700

    More registration

--- src/main/java/com/verdantartifice/thaumicwonders/common/init/InitVoidBeacon.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/init/InitVoidBeacon.java
@@ -3,6 +3,7 @@ package com.verdantartifice.thaumicwonders.common.init;
 import com.verdantartifice.thaumicwonders.common.tiles.devices.TileVoidBeacon;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class InitVoidBeacon {
@@ -40,6 +41,32 @@ public class InitVoidBeacon {
         TileVoidBeacon.registerOreDict("dirt");
         TileVoidBeacon.registerItemStack(new ItemStack(Blocks.DIRT, 1, 2));
         TileVoidBeacon.registerOreDict("sand");
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.SANDSTONE));
         TileVoidBeacon.registerOreDict("grass");
+        TileVoidBeacon.registerOreDict("endstone");
+        TileVoidBeacon.registerOreDict("gravel");
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.MYCELIUM));
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.CLAY));
+        TileVoidBeacon.registerOreDict("netherrack");
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.SOUL_SAND));
+        
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.MOSSY_COBBLESTONE));
+        TileVoidBeacon.registerOreDict("obsidian");
+        
+        TileVoidBeacon.registerOreDict("logWood");
+        TileVoidBeacon.registerOreDict("treeSapling");
+        TileVoidBeacon.registerOreDict("treeLeaves");
+        
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.TALLGRASS));
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.DOUBLE_PLANT, 1, 32767));
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.WATERLILY));
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.DEADBUSH));
+        TileVoidBeacon.registerOreDict("vine");
+        TileVoidBeacon.registerItemStack(new ItemStack(Items.WHEAT_SEEDS));
+        TileVoidBeacon.registerItemStack(new ItemStack(Items.MELON_SEEDS));
+        TileVoidBeacon.registerItemStack(new ItemStack(Items.PUMPKIN_SEEDS));
+        TileVoidBeacon.registerItemStack(new ItemStack(Items.BEETROOT_SEEDS));
+        TileVoidBeacon.registerOreDict("cropNetherWart");
+
     }
 }
