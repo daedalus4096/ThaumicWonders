7e3c7c83599fd50cab5cf2d59dc2ce5ff001292d
Michael Bunting <michael.a.bunting@gmail.com>
2019-06-08 17:55:58 -0700

    Initial registration

--- src/main/java/com/verdantartifice/thaumicwonders/common/init/InitVoidBeacon.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/init/InitVoidBeacon.java
@@ -2,6 +2,9 @@ package com.verdantartifice.thaumicwonders.common.init;
 
 import com.verdantartifice.thaumicwonders.common.tiles.devices.TileVoidBeacon;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+
 public class InitVoidBeacon {
     public static void init() {
         TileVoidBeacon.initRegistry();
@@ -9,7 +12,34 @@ public class InitVoidBeacon {
     }
     
     private static void registerEntries() {
+        TileVoidBeacon.registerOreDict("oreLapis");
+        TileVoidBeacon.registerOreDict("oreDiamond");
+        TileVoidBeacon.registerOreDict("oreRedstone");
+        TileVoidBeacon.registerOreDict("oreEmerald");
+        TileVoidBeacon.registerOreDict("oreQuartz");
+        TileVoidBeacon.registerOreDict("oreIron");
+        TileVoidBeacon.registerOreDict("oreGold");
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.COAL_ORE));
+        TileVoidBeacon.registerOreDict("glowstone");
+        
+        TileVoidBeacon.registerOreDict("oreCopper");
+        TileVoidBeacon.registerOreDict("oreTin");
+        TileVoidBeacon.registerOreDict("oreSilver");
+        TileVoidBeacon.registerOreDict("oreLead");
+        
+        TileVoidBeacon.registerOreDict("oreUranium");
+        TileVoidBeacon.registerOreDict("oreRuby");
+        TileVoidBeacon.registerOreDict("oreGreenSapphire");
+        TileVoidBeacon.registerOreDict("oreSapphire");
+        
         TileVoidBeacon.registerOreDict("stone");
+        TileVoidBeacon.registerOreDict("stoneGranite");
+        TileVoidBeacon.registerOreDict("stoneDiorite");
+        TileVoidBeacon.registerOreDict("stoneAndesite");
         TileVoidBeacon.registerOreDict("cobblestone");
+        TileVoidBeacon.registerOreDict("dirt");
+        TileVoidBeacon.registerItemStack(new ItemStack(Blocks.DIRT, 1, 2));
+        TileVoidBeacon.registerOreDict("sand");
+        TileVoidBeacon.registerOreDict("grass");
     }
 }
