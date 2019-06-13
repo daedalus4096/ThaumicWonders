7ec976a1fc4ccc9137eeb77d2beef58c940a7ae9
Michael Bunting <michael.a.bunting@gmail.com>
2019-06-12 23:10:30 -0700

    Auto fire

--- src/main/java/com/verdantartifice/thaumicwonders/common/items/tools/ItemBoneBow.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/items/tools/ItemBoneBow.java
@@ -5,11 +5,19 @@ import javax.annotation.Nullable;
 import com.verdantartifice.thaumicwonders.ThaumicWonders;
 import com.verdantartifice.thaumicwonders.common.items.ItemsTW;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.IItemPropertyGetter;
+import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 
 public class ItemBoneBow extends ItemBow {
@@ -26,16 +34,111 @@ public class ItemBoneBow extends ItemBow {
                 if (entityIn == null) {
                     return 0.0F;
                 } else {
-                    return entityIn.getActiveItemStack().getItem() != ItemsTW.BONE_BOW ? 0.0F : (float)(stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F;
+                    return entityIn.getActiveItemStack().getItem() != ItemsTW.BONE_BOW ? 0.0F : (float)(stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / (float)(stack.getMaxItemUseDuration());
                 }
             }
         });
     }
     
     @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 5;
+    }
+    
+    @Override
     public void onUsingTick(ItemStack stack, EntityLivingBase player, int count) {
-        // TODO Auto-generated method stub
-//        player.stopActiveHand();
-        super.onUsingTick(stack, player, count);
+        if (count <= 1) {
+            player.stopActiveHand();
+        }
+    }
+    
+    public static float getArrowVelocity(int charge) {
+        float f = (float)charge / 4.0F;
+        f = (f * f + f * 2.0F) / 3.0F;
+        if (f > 1.0F) {
+            f = 1.0F;
+        }
+        return f;
+    }
+    
+    protected ItemStack findAmmo(EntityPlayer player) {
+        if (this.isArrow(player.getHeldItemOffhand())) {
+            return player.getHeldItemOffhand();
+        } else if (this.isArrow(player.getHeldItemMainhand())) {
+            return player.getHeldItemMainhand();
+        } else {
+            for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+                ItemStack itemstack = player.inventory.getStackInSlot(i);
+                if (this.isArrow(itemstack)) {
+                    return itemstack;
+                }
+            }
+            return ItemStack.EMPTY;
+        }
+    }
+    
+    @Override
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft) {
+        if (entityLiving instanceof EntityPlayer) {
+            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
+            boolean flag = entityplayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack) > 0;
+            ItemStack itemstack = this.findAmmo(entityplayer);
+
+            int charge = this.getMaxItemUseDuration(stack) - timeLeft;
+            charge = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, worldIn, entityplayer, charge, !itemstack.isEmpty() || flag);
+            if (charge < 0) {
+                return;
+            }
+
+            if (!itemstack.isEmpty() || flag) {
+                if (itemstack.isEmpty()) {
+                    itemstack = new ItemStack(Items.ARROW);
+                }
+
+                float velocity = getArrowVelocity(charge);
+                if ((double)velocity >= 0.1D) {
+                    boolean flag1 = entityplayer.capabilities.isCreativeMode || (itemstack.getItem() instanceof ItemArrow && ((ItemArrow) itemstack.getItem()).isInfinite(itemstack, stack, entityplayer));
+                    if (!worldIn.isRemote) {
+                        ItemArrow itemarrow = (ItemArrow)(itemstack.getItem() instanceof ItemArrow ? itemstack.getItem() : Items.ARROW);
+                        EntityArrow entityarrow = itemarrow.createArrow(worldIn, itemstack, entityplayer);
+                        entityarrow.shoot(entityplayer, entityplayer.rotationPitch, entityplayer.rotationYaw, 0.0F, velocity * 4.0F, 1.0F);
+
+                        if (velocity == 1.0F) {
+                            entityarrow.setIsCritical(true);
+                        }
+
+                        int powerLevels = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
+                        if (powerLevels > 0) {
+                            entityarrow.setDamage(entityarrow.getDamage() + (double)powerLevels * 0.5D + 0.5D);
+                        }
+
+                        int punchLevels = EnchantmentHelper.getEnchantmentLevel(Enchantments.PUNCH, stack);
+                        if (punchLevels > 0) {
+                            entityarrow.setKnockbackStrength(punchLevels);
+                        }
+
+                        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FLAME, stack) > 0) {
+                            entityarrow.setFire(100);
+                        }
+
+                        stack.damageItem(1, entityplayer);
+                        if (flag1 || entityplayer.capabilities.isCreativeMode && (itemstack.getItem() == Items.SPECTRAL_ARROW || itemstack.getItem() == Items.TIPPED_ARROW)) {
+                            entityarrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
+                        }
+
+                        worldIn.spawnEntity(entityarrow);
+                    }
+
+                    worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + velocity * 0.5F);
+
+                    if (!flag1 && !entityplayer.capabilities.isCreativeMode) {
+                        itemstack.shrink(1);
+                        if (itemstack.isEmpty()) {
+                            entityplayer.inventory.deleteStack(itemstack);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
