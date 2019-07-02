2cc2bed93b064c64f15c45e7286098651d027412
Michael Bunting <michael.a.bunting@gmail.com>
2019-07-01 22:06:19 -0700

    Accelerator GUI

--- src/main/java/com/verdantartifice/thaumicwonders/common/blocks/devices/BlockPrimordialAccelerator.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/blocks/devices/BlockPrimordialAccelerator.java
@@ -1,15 +1,20 @@
 package com.verdantartifice.thaumicwonders.common.blocks.devices;
 
+import com.verdantartifice.thaumicwonders.ThaumicWonders;
 import com.verdantartifice.thaumicwonders.common.blocks.base.BlockDeviceTW;
+import com.verdantartifice.thaumicwonders.common.misc.GuiIds;
 import com.verdantartifice.thaumicwonders.common.tiles.devices.TilePrimordialAccelerator;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import thaumcraft.common.blocks.IBlockEnabled;
 import thaumcraft.common.blocks.IBlockFacingHorizontal;
 
@@ -37,5 +42,13 @@ public class BlockPrimordialAccelerator extends BlockDeviceTW<TilePrimordialAcce
         } else {
             return BlockFaceShape.UNDEFINED;
         }
+    }
+    
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (!worldIn.isRemote) {
+            playerIn.openGui(ThaumicWonders.INSTANCE, GuiIds.PRIMORDIAL_ACCELERATOR, worldIn, pos.getX(), pos.getY(), pos.getZ());
+        }
+        return true;
     }
 }
--- src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TilePrimordialAccelerator.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TilePrimordialAccelerator.java
@@ -1,11 +1,19 @@
 package com.verdantartifice.thaumicwonders.common.tiles.devices;
 
-import com.verdantartifice.thaumicwonders.common.tiles.base.TileTW;
+import com.verdantartifice.thaumicwonders.common.tiles.base.TileTWInventory;
 
 import net.minecraft.util.ITickable;
 
-public class TilePrimordialAccelerator extends TileTW implements ITickable {
-
+public class TilePrimordialAccelerator extends TileTWInventory implements ITickable {
+    public TilePrimordialAccelerator() {
+        super(1);
+    }
+    
+    @Override
+    public int getInventoryStackLimit() {
+        return 1;
+    }
+    
     @Override
     public void update() {
         // TODO Auto-generated method stub
