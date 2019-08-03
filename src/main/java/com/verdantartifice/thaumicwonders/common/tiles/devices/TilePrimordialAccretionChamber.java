c4121a7ef7718594b00f57cd81194ef74ceeb2d8
Michael Bunting <michael.a.bunting@gmail.com>
2019-08-02 23:22:17 -0700

    Add essentia capabilities to chamber

--- src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TilePrimordialAccretionChamber.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/tiles/devices/TilePrimordialAccretionChamber.java
@@ -1,13 +1,23 @@
 package com.verdantartifice.thaumicwonders.common.tiles.devices;
 
+import com.verdantartifice.thaumicwonders.ThaumicWonders;
 import com.verdantartifice.thaumicwonders.common.tiles.base.TileTWInventory;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.aspects.IAspectContainer;
 
-public class TilePrimordialAccretionChamber extends TileTWInventory implements ITickable {
+public class TilePrimordialAccretionChamber extends TileTWInventory implements IAspectContainer, ITickable {
+    private static final int CAPACITY = 125;
+    private static final AspectList REQUIRED_FUEL = new AspectList().add(Aspect.AIR, 125).add(Aspect.EARTH, 125).add(Aspect.FIRE, 125).add(Aspect.WATER, 125).add(Aspect.ORDER, 125).add(Aspect.ENTROPY, 125);
+    
+    protected AspectList essentia = new AspectList();
+    
     public TilePrimordialAccretionChamber() {
         super(32);
     }
@@ -20,10 +30,115 @@ public class TilePrimordialAccretionChamber extends TileTWInventory implements I
             this.getPos().getX() + 2.3D, this.getPos().getY() + 2.3D, this.getPos().getZ() + 1.3D
         );
     }
+    
+    @Override
+    protected void readFromTileNBT(NBTTagCompound compound) {
+        this.essentia.readFromNBT(compound, "essentia");
+    }
+    
+    @Override
+    protected NBTTagCompound writeToTileNBT(NBTTagCompound compound) {
+        this.essentia.writeToNBT(compound, "essentia");
+        return compound;
+    }
 
     @Override
     public void update() {
         // TODO Auto-generated method stub
 
+    }
+    
+    public Aspect getCurrentSuction() {
+        for (Aspect aspect : REQUIRED_FUEL.getAspectsSortedByName()) {
+            if (!this.doesContainerContainAmount(aspect, CAPACITY)) {
+                return aspect;
+            }
+        }
+        return null;
+    }
+    
+    public boolean isEssentiaFull() {
+        return this.doesContainerContain(REQUIRED_FUEL);
+    }
+
+    @Override
+    public int addToContainer(Aspect aspect, int toAdd) {
+        if (toAdd == 0) {
+            return 0;
+        } else if (this.essentia.getAmount(aspect) >= CAPACITY || !this.doesContainerAccept(aspect)) {
+            // Incompatible addition; return all of it
+            this.syncTile(false);
+            this.markDirty();
+            return toAdd;
+        } else {
+            // Add as much as possible and return the remainder
+            int added = Math.min(toAdd, CAPACITY - this.essentia.getAmount(aspect));
+            this.essentia.add(aspect, added);
+            this.syncTile(false);
+            this.markDirty();
+            ThaumicWonders.LOGGER.info("Added {} {} to central accretion chamber, new total {}", added, aspect.getName(), this.essentia.getAmount(aspect));
+            return (toAdd - added);
+        }
+    }
+
+    @Override
+    public int containerContains(Aspect aspect) {
+        return this.doesContainerAccept(aspect) ? this.essentia.getAmount(aspect) : 0;
+    }
+
+    @Override
+    public boolean doesContainerAccept(Aspect aspect) {
+        return (REQUIRED_FUEL.getAmount(aspect) > 0);
+    }
+
+    @Override
+    public boolean doesContainerContain(AspectList aspectList) {
+        boolean satisfied = true;
+        for (Aspect aspect : aspectList.getAspects()) {
+            satisfied = satisfied && this.doesContainerContainAmount(aspect, aspectList.getAmount(aspect));
+        }
+        return satisfied;
+    }
+
+    @Override
+    public boolean doesContainerContainAmount(Aspect aspect, int amt) {
+        return this.essentia.getAmount(aspect) >= amt;
+    }
+
+    @Override
+    public AspectList getAspects() {
+        return this.essentia.copy();
+    }
+
+    @Override
+    public void setAspects(AspectList aspects) {
+        if (aspects != null) {
+            this.essentia = aspects.copy();
+        }
+    }
+
+    @Override
+    public boolean takeFromContainer(AspectList aspectList) {
+        if (!this.doesContainerContain(aspectList)) {
+            return false;
+        } else {
+            boolean satisfied = true;
+            for (Aspect aspect : aspectList.getAspects()) {
+                satisfied = satisfied && this.takeFromContainer(aspect, aspectList.getAmount(aspect));
+            }
+            return satisfied;
+        }
+    }
+
+    @Override
+    public boolean takeFromContainer(Aspect aspect, int amt) {
+        if (this.doesContainerContainAmount(aspect, amt)) {
+            this.essentia.reduce(aspect, amt);
+            this.syncTile(false);
+            this.markDirty();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/com/verdantartifice/thaumicwonders/common/tiles/misc/TilePrimordialAccretionChamberPlaceholder.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/tiles/misc/TilePrimordialAccretionChamberPlaceholder.java
@@ -1,7 +1,245 @@
 package com.verdantartifice.thaumicwonders.common.tiles.misc;
 
-import com.verdantartifice.thaumicwonders.common.tiles.base.TileTW;
+import javax.annotation.Nullable;
 
-public class TilePrimordialAccretionChamberPlaceholder extends TileTW {
+import com.verdantartifice.thaumicwonders.ThaumicWonders;
+import com.verdantartifice.thaumicwonders.common.tiles.base.TileTW;
+import com.verdantartifice.thaumicwonders.common.tiles.devices.TilePrimordialAccretionChamber;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import thaumcraft.api.ThaumcraftApiHelper;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.aspects.IAspectContainer;
+import thaumcraft.api.aspects.IEssentiaTransport;
+import thaumcraft.api.aura.AuraHelper;
+
+public class TilePrimordialAccretionChamberPlaceholder extends TileTW implements IAspectContainer, IEssentiaTransport, ITickable {
+    protected int tickCounter = 0;
+    
+    @Override
+    public void update() {
+        if (!this.world.isRemote && (++this.tickCounter % 5 == 0)) {
+            TilePrimordialAccretionChamber tile = this.findCentralChamber();
+            if (tile != null && !tile.isEssentiaFull()) {
+                this.fill();
+            }
+        }
+    }
+    
+    protected void fill() {
+        TilePrimordialAccretionChamber centralTile = this.findCentralChamber();
+        if (centralTile == null) {
+            return;
+        }
+        for (EnumFacing face : EnumFacing.VALUES) {
+            if (!this.canInputFrom(face)) {
+                continue;
+            }
+            TileEntity te = ThaumcraftApiHelper.getConnectableTile(this.world, this.pos, face);
+            if (te != null && te instanceof IEssentiaTransport) {
+                IEssentiaTransport otherTile = (IEssentiaTransport)te;
+                if (!otherTile.canOutputTo(face.getOpposite())) {
+                    continue;
+                }
+                Aspect currentSuction = centralTile.getCurrentSuction();
+                if ( otherTile.getEssentiaType(face.getOpposite()) == currentSuction &&
+                     otherTile.getEssentiaAmount(face.getOpposite()) > 0 &&
+                     this.getSuctionAmount(face) > otherTile.getSuctionAmount(face.getOpposite()) &&
+                     this.getSuctionAmount(face) >= otherTile.getMinimumSuction() ) {
+                    int taken = otherTile.takeEssentia(currentSuction, 1, face.getOpposite());
+                    int leftover = this.addToContainer(currentSuction, taken);
+                    if (leftover > 0) {
+                        ThaumicWonders.LOGGER.info("Primordial accretion chamber spilling {} essentia on fill", leftover);
+                        AuraHelper.polluteAura(this.world, this.pos, leftover, true);
+                    }
+                    centralTile.syncTile(false);
+                    centralTile.markDirty();
+                    if (centralTile.isEssentiaFull()) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    @Nullable
+    protected TilePrimordialAccretionChamber findCentralChamber() {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                for (int k = -1; k <= 1; k++) {
+                    BlockPos searchPos = this.pos.add(i, j, k);
+                    TileEntity tile = this.world.getTileEntity(searchPos);
+                    if (tile instanceof TilePrimordialAccretionChamber) {
+                        return (TilePrimordialAccretionChamber)tile;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+    protected boolean isEssentiaPort(EnumFacing face) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        if (tile == null) {
+            return false;
+        } else {
+            EnumFacing facing = tile.getFacing();
+            for (int offset = -1; offset <= 1; offset++) {
+                EnumFacing leftFace = facing.rotateYCCW();
+                BlockPos leftPos = tile.getPos().offset(facing, offset).offset(leftFace);
+                if (leftPos.equals(this.pos)) {
+                    return (face == leftFace);
+                }
+                
+                EnumFacing rightFace = facing.rotateY();
+                BlockPos rightPos = tile.getPos().offset(facing, offset).offset(rightFace);
+                if (rightPos.equals(this.pos)) {
+                    return (face == rightFace);
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int addToContainer(Aspect aspect, int toAdd) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? 0 : tile.addToContainer(aspect, toAdd);
+    }
+
+    @Override
+    public int containerContains(Aspect aspect) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? 0 : tile.containerContains(aspect);
+    }
+
+    @Override
+    public boolean doesContainerAccept(Aspect aspect) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? false : tile.doesContainerAccept(aspect);
+    }
+
+    @Override
+    public boolean doesContainerContain(AspectList aspectList) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? false : tile.doesContainerContain(aspectList);
+    }
+
+    @Override
+    public boolean doesContainerContainAmount(Aspect aspect, int amt) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? false : tile.doesContainerContainAmount(aspect, amt);
+    }
+
+    @Override
+    public AspectList getAspects() {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? new AspectList() : tile.getAspects();
+    }
+
+    @Override
+    public void setAspects(AspectList aspects) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        if (tile != null) {
+            tile.setAspects(aspects);
+        }
+    }
+
+    @Override
+    public boolean takeFromContainer(AspectList aspectList) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? false : tile.takeFromContainer(aspectList);
+    }
+
+    @Override
+    public boolean takeFromContainer(Aspect aspect, int amt) {
+        TilePrimordialAccretionChamber tile = this.findCentralChamber();
+        return tile == null ? false : tile.takeFromContainer(aspect, amt);
+    }
+
+    @Override
+    public int addEssentia(Aspect aspect, int amt, EnumFacing face) {
+        if (this.canInputFrom(face)) {
+            return (amt - this.addToContainer(aspect, amt));
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean canInputFrom(EnumFacing face) {
+        return this.isConnectable(face);
+    }
+
+    @Override
+    public boolean canOutputTo(EnumFacing face) {
+        return false;
+    }
+
+    @Override
+    public int getEssentiaAmount(EnumFacing face) {
+        Aspect type = this.getEssentiaType(face);
+        if (type == null) {
+            return 0;
+        } else {
+            return this.getAspects().getAmount(type);
+        }
+    }
+
+    @Override
+    public Aspect getEssentiaType(EnumFacing face) {
+        AspectList aspects = this.getAspects();
+        if (aspects == null || aspects.size() == 0) {
+            return null;
+        } else {
+            return aspects.getAspectsSortedByAmount()[0];
+        }
+    }
+
+    @Override
+    public int getMinimumSuction() {
+        // Can't output, so no need for minimum suction
+        return 0;
+    }
+
+    @Override
+    public int getSuctionAmount(EnumFacing face) {
+        if (this.isEssentiaPort(face)) {
+            Aspect type = this.getSuctionType(face);
+            return type == null ? 0 : 128;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public Aspect getSuctionType(EnumFacing face) {
+        if (this.isEssentiaPort(face)) {
+            TilePrimordialAccretionChamber tile = this.findCentralChamber();
+            return tile == null ? null : tile.getCurrentSuction();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isConnectable(EnumFacing face) {
+        return this.isEssentiaPort(face);
+    }
+
+    @Override
+    public void setSuction(Aspect aspect, int amt) {
+        // Do nothing
+    }
+
+    @Override
+    public int takeEssentia(Aspect aspect, int amt, EnumFacing face) {
+        // Can't output
+        return 0;
+    }
 
 }
